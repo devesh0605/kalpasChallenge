8f61ceb6bd465d734cf94abd29427caeb34bccf0
devesh0605 <dsaxena_be18@thapar.edu>
2021-06-04 13:57:04 +0530

    final tweeks

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -42,205 +42,208 @@ class SignUp extends StatelessWidget {
                     ),
                   ),
                   child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(25.0),
-                          child: Text(
-                            'Sign Up',
-                            style: TextStyle(
-                                fontSize: 30, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 30.0, vertical: 5.0),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(25.0),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: Colors.brown[900], width: 2.0),
-                                borderRadius: BorderRadius.circular(25.0),
-                              ),
-                              contentPadding: EdgeInsets.symmetric(
-                                vertical: 15.0,
-                                horizontal: 20,
-                              ),
-                              fillColor: Colors.brown[200],
-                              filled: true,
-                              hintText: 'Email:',
-                              hintStyle: TextStyle(
-                                color: Colors.white,
-                              ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(25.0),
+                            child: Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.bold),
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 30.0, vertical: 5.0),
-                          child: TextField(
-                            keyboardType: TextInputType.emailAddress,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(25.0),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: Colors.brown[900], width: 2.0),
-                                borderRadius: BorderRadius.circular(25.0),
-                              ),
-                              contentPadding: EdgeInsets.symmetric(
-                                vertical: 15.0,
-                                horizontal: 20,
-                              ),
-                              fillColor: Colors.brown[200],
-                              filled: true,
-                              hintText: 'Password:',
-                              hintStyle: TextStyle(
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 30.0, vertical: 5.0),
-                          child: TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(25.0),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: Colors.brown[900], width: 2.0),
-                                borderRadius: BorderRadius.circular(25.0),
-                              ),
-                              contentPadding: EdgeInsets.symmetric(
-                                vertical: 15.0,
-                                horizontal: 20,
-                              ),
-                              fillColor: Colors.brown[200],
-                              filled: true,
-                              hintText: 'Re-enter Password',
-                              hintStyle: TextStyle(
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.blue[400],
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          child: GestureDetector(
-                            onTap: () {
-                              Navigator.push(context,
-                                  MaterialPageRoute(builder: (context) {
-                                return SignIn();
-                              }));
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 15,
-                                horizontal: 40,
-                              ),
-                              child: Text(
-                                'Sign Up',
-                                style: TextStyle(
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 30.0, vertical: 5.0),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.brown[900], width: 2.0),
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                                contentPadding: EdgeInsets.symmetric(
+                                  vertical: 15.0,
+                                  horizontal: 20,
+                                ),
+                                fillColor: Colors.brown[200],
+                                filled: true,
+                                hintText: 'Email:',
+                                hintStyle: TextStyle(
                                   color: Colors.white,
-                                  fontSize: 20,
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              width: 70,
-                              height: 2,
-                              color: Colors.black26,
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              'Or Sign Up With',
-                              style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 30.0, vertical: 5.0),
+                            child: TextField(
+                              keyboardType: TextInputType.emailAddress,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.brown[900], width: 2.0),
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                                contentPadding: EdgeInsets.symmetric(
+                                  vertical: 15.0,
+                                  horizontal: 20,
+                                ),
+                                fillColor: Colors.brown[200],
+                                filled: true,
+                                hintText: 'Password:',
+                                hintStyle: TextStyle(
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
-                            SizedBox(
-                              width: 10,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 30.0, vertical: 5.0),
+                            child: TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.brown[900], width: 2.0),
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                                contentPadding: EdgeInsets.symmetric(
+                                  vertical: 15.0,
+                                  horizontal: 20,
+                                ),
+                                fillColor: Colors.brown[200],
+                                filled: true,
+                                hintText: 'Re-enter Password',
+                                hintStyle: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
                             ),
-                            Container(
-                              width: 70,
-                              height: 2,
-                              color: Colors.black26,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.blue[400],
+                              borderRadius: BorderRadius.circular(30),
                             ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image(
-                              height: 50,
-                              width: 50,
-                              fit: BoxFit.cover,
-                              image: AssetImage('assets/images/google1.png'),
-                            ),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            Image(
-                              height: 50,
-                              width: 50,
-                              fit: BoxFit.cover,
-                              image: AssetImage('assets/images/facebook.png'),
-                            )
-                          ],
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text('Have an account?'),
-                            GestureDetector(
+                            child: GestureDetector(
                               onTap: () {
                                 Navigator.push(context,
                                     MaterialPageRoute(builder: (context) {
                                   return SignIn();
                                 }));
                               },
-                              child: Text(
-                                ' Sign in',
-                                style: TextStyle(
-                                  color: Colors.orange[700],
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 15,
+                                  horizontal: 40,
+                                ),
+                                child: Text(
+                                  'Sign Up',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
                                 ),
                               ),
                             ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                      ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                width: 70,
+                                height: 2,
+                                color: Colors.black26,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                'Or Sign Up With',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Container(
+                                width: 70,
+                                height: 2,
+                                color: Colors.black26,
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image(
+                                height: 50,
+                                width: 50,
+                                fit: BoxFit.cover,
+                                image: AssetImage('assets/images/google1.png'),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Image(
+                                height: 50,
+                                width: 50,
+                                fit: BoxFit.cover,
+                                image: AssetImage('assets/images/facebook.png'),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('Have an account?'),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(context,
+                                      MaterialPageRoute(builder: (context) {
+                                    return SignIn();
+                                  }));
+                                },
+                                child: Text(
+                                  ' Sign in',
+                                  style: TextStyle(
+                                    color: Colors.orange[700],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
