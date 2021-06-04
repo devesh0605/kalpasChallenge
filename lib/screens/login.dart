445bad0b9d5c54611ab209c1f289064ad9439b25
devesh0605 <dsaxena_be18@thapar.edu>
2021-06-04 13:34:02 +0530

    sign up screen complete

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -61,8 +61,8 @@ class LoginScreen extends StatelessWidget {
                                 borderRadius: BorderRadius.circular(25.0),
                               ),
                               focusedBorder: OutlineInputBorder(
-                                borderSide: const BorderSide(
-                                    color: Colors.green, width: 2.0),
+                                borderSide: BorderSide(
+                                    color: Colors.brown[900], width: 2.0),
                                 borderRadius: BorderRadius.circular(25.0),
                               ),
                               contentPadding: EdgeInsets.symmetric(
@@ -88,8 +88,8 @@ class LoginScreen extends StatelessWidget {
                                 borderRadius: BorderRadius.circular(25.0),
                               ),
                               focusedBorder: OutlineInputBorder(
-                                borderSide: const BorderSide(
-                                    color: Colors.green, width: 2.0),
+                                borderSide: BorderSide(
+                                    color: Colors.brown[900], width: 2.0),
                                 borderRadius: BorderRadius.circular(25.0),
                               ),
                               contentPadding: EdgeInsets.symmetric(
@@ -115,8 +115,8 @@ class LoginScreen extends StatelessWidget {
                                 borderRadius: BorderRadius.circular(25.0),
                               ),
                               focusedBorder: OutlineInputBorder(
-                                borderSide: const BorderSide(
-                                    color: Colors.green, width: 2.0),
+                                borderSide: BorderSide(
+                                    color: Colors.brown[900], width: 2.0),
                                 borderRadius: BorderRadius.circular(25.0),
                               ),
                               contentPadding: EdgeInsets.symmetric(
@@ -131,6 +131,102 @@ class LoginScreen extends StatelessWidget {
                               ),
                             ),
                           ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.blue[400],
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: GestureDetector(
+                            onTap: () {
+                              print('Something cool will happen');
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 15,
+                                horizontal: 40,
+                              ),
+                              child: Text(
+                                'Sign Up',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 70,
+                              height: 2,
+                              color: Colors.black26,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              'Or Sign Up With',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Container(
+                              width: 70,
+                              height: 2,
+                              color: Colors.black26,
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image(
+                              height: 50,
+                              width: 50,
+                              fit: BoxFit.cover,
+                              image: AssetImage('assets/images/google1.png'),
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Image(
+                              height: 50,
+                              width: 50,
+                              fit: BoxFit.cover,
+                              image: AssetImage('assets/images/facebook.png'),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Have an account?'),
+                            Text(
+                              ' Sign in',
+                              style: TextStyle(
+                                color: Colors.orange[700],
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
                         ),
                       ],
                     ),
